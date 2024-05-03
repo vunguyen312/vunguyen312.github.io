0a209af4e0133052ff1ed6d60dfc155127334624
vunguyen312 <vunguyen312@outlook.com>
2024-05-02 21:33:12 -0400

    Split SplitTitle into its separate file and fixed typewriter effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,106 +1,8 @@
-import React, {useEffect, useState} from "react";
 import {Col, Row, Container} from "react-bootstrap";
 import Projects from "./Projects";
 import {AiFillGithub} from "react-icons/ai";
 import {FaLinkedinIn} from "react-icons/fa";
-
-
-function SetTitle(){
-    const defaultText = 'Vu Nguyen';
-    const newText = 'Software Developer';
-    const [text, setText] = useState(defaultText);
-    const [isAnimating, setIsAnimating] = useState(false);
-    const [blink, setBlink] = useState("_");
-
-    function BlinkUnderscore(){
-    
-        useEffect(() => {
-            const interval = setInterval(() => {
-            const newState = blink === "_" ? "" : "_";
-            setBlink(newState);
-            }, 500);
-            return () => clearInterval(interval);
-        });
-    
-        return(blink);
-    }
-
-    const typeName = (str) => {
-
-        if(isAnimating) return;
-
-        let counter = 0;
-
-        setIsAnimating(true);
-        setBlink("|");
-
-        const interval = setInterval(() => {
-
-            setText(defaultText.substring(0, counter));
-
-            if(counter < defaultText.length) return counter++;
-
-            clearInterval(interval);
-            setIsAnimating(false);
-
-        }, 70);
-    }
-
-    const resetText = (str) => {
-        
-        if(isAnimating) return;
-
-        let counter = str.length;
-
-        setIsAnimating(true);
-        setBlink("|");
-
-        const interval = setInterval(() => {
-
-            setText(str.substring(0, counter));
-
-            if(counter > 0) return counter--;
-
-            clearInterval(interval);
-            setIsAnimating(false);
-            if(str === newText) return typeName();
-            typeTitle();
-
-        }, 70);
-    }
-
-    const typeTitle = () => {
-        
-        if(isAnimating || text === newText) return;
-        
-        let counter = 1;
-
-        setIsAnimating(true);
-        setBlink("|");
-
-        const interval = setInterval(() => {
-
-            setText(newText.substring(0, counter));
-
-            if(counter < newText.length) return counter++;
-
-            clearInterval(interval);
-            setIsAnimating(false);
-            setTimeout(() => resetText(newText), 3000);
-
-        }, 50);
-    }
-    
-    const animationPlayState = text === newText ? 'paused' : 'initial';
-
-    return(
-        <span style={{animationPlayState: animationPlayState}} className="json" 
-        onMouseEnter={() => resetText(defaultText)}
-        >
-            {text}<BlinkUnderscore />
-        </span>
-    );
-}
+import SetTitle from "./vfx/SetTitle";
 
 function Home() {
     return(
@@ -119,7 +21,7 @@ function Home() {
                                     rel="noreferrer"
                                     className="home-social-icons"
                                 >
-                                    <AiFillGithub/>
+                                    <AiFillGithub />
                                 </a>
                             </li>
                             <li className="social-icons">
@@ -129,7 +31,7 @@ function Home() {
                                     rel="noreferrer"
                                     className="home-social-icons"
                                 >
-                                    <FaLinkedinIn/>
+                                    <FaLinkedinIn />
                                 </a>
                             </li>
                         </ul>
