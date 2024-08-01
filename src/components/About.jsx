8e48f2f156c0150d6a6a5f35a84fb2044bd3e6ed
vunguyen312 <vunguyen312@outlook.com>
2024-07-31 21:48:02 -0400

    Fixed auto-scrolling for smaller screens on carousel

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Container } from "react-bootstrap";
 import Carousel from "react-elastic-carousel";
 import SkillTemplate from "./SkillTemplate";
@@ -19,10 +19,22 @@ import Unity from "../assets/unity.png";
 import VSCode from "../assets/vscode.png";
 
 function About() {
-    const breakPoints = [
-        { width: 1, itemsToShow: 1 },
-        { width: 550, itemsToShow: 2 }
-    ];
+    const [visibleItems, setVisibleItems] = useState(0);
+
+    const updateVisibleItems = () => {
+        const width = window.innerWidth;
+        if(width < 700) setVisibleItems(2);
+        else if(width < 1000) setVisibleItems(3);
+        else if(width < 1300) setVisibleItems(4);
+        else setVisibleItems(5);
+    };
+
+    useEffect(() => {
+        updateVisibleItems();
+        window.addEventListener('resize', updateVisibleItems);
+        return () => window.removeEventListener('resize', updateVisibleItems);
+      }, []);
+    
     
     const carouselRef = React.useRef(null);
     const [scrollTop, setScrollTop] = React.useState(false);
@@ -35,11 +47,17 @@ function About() {
     const onPrevStart = (currentItem, nextItem) => {
         if(currentItem.index !== nextItem.index) return;
         setScrollTop(true);
-        carouselRef.current.goTo(15);
+        carouselRef.current.goTo(13);
     };
     const onNextEnd = (currentItem) => {
+        console.log();
         clearTimeout(resetTimeout);
-        if(currentItem.index < 13) return;
+        if(
+            currentItem.index < 10 & visibleItems === 5
+            || currentItem.index < 11 & visibleItems === 4
+            || currentItem.index < 12 & visibleItems === 3
+            || currentItem.index < 13 & visibleItems === 2
+        ) return;
         if(scrollTop) return setScrollTop(false);
         resetTimeout = setTimeout(() => {
             carouselRef?.current?.goTo(0);
@@ -49,39 +67,37 @@ function About() {
     return (
         <div>
             <Container fluid className="about-section">
+                <p className="about-body">My Skills</p>
                 <Row>
-                    <h1>My Skills</h1>
+                    <Carousel 
+                        className="skill-carousel"
+                        itemsToShow={visibleItems}
+                        enableAutoPlay
+                        autoPlaySpeed={2000}
+                        enableMouseSwipe={false}
+                        ref={carouselRef}
+                        onNextStart={onNextStart}
+                        onNextEnd={onNextEnd}
+                        onPrevStart={onPrevStart}
+                        disableArrowsOnEnd={false}
+                    >
+                        <SkillTemplate imgPath={Arduino} title="Arduino" />
+                        <SkillTemplate imgPath={Bitbucket} title="Bitbucket" />
+                        <SkillTemplate imgPath={CSS} title="CSS" />
+                        <SkillTemplate imgPath={Git} title="Git" />
+                        <SkillTemplate imgPath={HTML} title="HTML" />
+                        <SkillTemplate imgPath={JavaScript} title="JavaScript" />
+                        <SkillTemplate imgPath={Lua} title="Lua" />
+                        <SkillTemplate imgPath={MongoDB} title="MongoDB" />
+                        <SkillTemplate imgPath={NodeJs} title="NodeJS" />
+                        <SkillTemplate imgPath={npm} title="npm" />
+                        <SkillTemplate imgPath={Playwright} title="Playwright" />
+                        <SkillTemplate imgPath={ReactPNG} title="React" />
+                        <SkillTemplate imgPath={TypeScript} title="TypeScript" />
+                        <SkillTemplate imgPath={Unity} title="Unity" />
+                        <SkillTemplate imgPath={VSCode} title="Visual Studio Code" />
+                    </Carousel>
                 </Row>
-                <Carousel 
-                    className="skill-carousel"
-                    verticalMode
-                    breakPoints={breakPoints} 
-                    enableAutoPlay
-                    autoPlaySpeed={2000}
-                    enableMouseSwipe={false}
-                    ref={carouselRef}
-                    onNextStart={onNextStart}
-                    onNextEnd={onNextEnd}
-                    onPrevStart={onPrevStart}
-                    showArrows={false}
-                    itemPadding={[0, 0, 20, 0]}
-                >
-                    <SkillTemplate imgPath={Arduino} title="Arduino" />
-                    <SkillTemplate imgPath={Bitbucket} title="Bitbucket" />
-                    <SkillTemplate imgPath={CSS} title="CSS" />
-                    <SkillTemplate imgPath={Git} title="Git" />
-                    <SkillTemplate imgPath={HTML} title="HTML" />
-                    <SkillTemplate imgPath={JavaScript} title="JavaScript" />
-                    <SkillTemplate imgPath={Lua} title="Lua" />
-                    <SkillTemplate imgPath={MongoDB} title="MongoDB" />
-                    <SkillTemplate imgPath={NodeJs} title="NodeJS" />
-                    <SkillTemplate imgPath={npm} title="npm" />
-                    <SkillTemplate imgPath={Playwright} title="Playwright" />
-                    <SkillTemplate imgPath={ReactPNG} title="React" />
-                    <SkillTemplate imgPath={TypeScript} title="TypeScript" />
-                    <SkillTemplate imgPath={Unity} title="Unity" />
-                    <SkillTemplate imgPath={VSCode} title="Visual Studio Code" />
-                </Carousel>
             </Container>
         </div>
     );
